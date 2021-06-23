5df46d93303140ecc03dc11a6ad77c9de56e556e
Khoroshavin <khoroshavin.russia@gmail.com>
2021-06-23 21:26:34 +0200

    header ready

--- js/main.js
+++ js/main.js
@@ -1,44 +1,62 @@
-const swiper = new Swiper('.swiper-container_news', {
-  // Optional parameters
-  loop: true,
+document.addEventListener("DOMContentLoaded", function(event) {
 
-  // Pagination
-  pagination: {
-    el: '.swiper-pagination',
-  },
+  const burgerBtn   = document.querySelector('.burger__btn_input');
+  const headerBot   = document.querySelector('.header__bot');
+  
+  openBurgerMenu = () => {
+    burgerBtn.addEventListener('click', () => {
 
-  //
-  slidesPerView: 4,
-  // spaceBetween: 10,
-  // Responsive breakpoints
-  breakpoints: {
-    320: {
-      slidesPerView: 1,
-      // spaceBetween: 20
-    },
-    576: {
-      slidesPerView: 2,
-      // spaceBetween: 20
-    },
-    768: {
-      slidesPerView: 4,
-      // spaceBetween: 20
-    },
-    992: {
-      slidesPerView: 4,
-      spaceBetween: 10
-    },
-    // 1150: {
-    //   slidesPerView: 5,
-    //   spaceBetween: 20
-    // },
+      headerBot.classList.toggle("header__bot_open");
+      
+    });
 
-  },
+  };
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
+  openBurgerMenu();
 
 });
+
+// const swiper = new Swiper('.swiper-container_news', {
+//   // Optional parameters
+//   loop: true,
+
+//   // Pagination
+//   pagination: {
+//     el: '.swiper-pagination',
+//   },
+
+//   //
+//   slidesPerView: 4,
+//   // spaceBetween: 10,
+//   // Responsive breakpoints
+//   breakpoints: {
+//     320: {
+//       slidesPerView: 1,
+//       // spaceBetween: 20
+//     },
+//     576: {
+//       slidesPerView: 2,
+//       // spaceBetween: 20
+//     },
+//     768: {
+//       slidesPerView: 4,
+//       // spaceBetween: 20
+//     },
+//     992: {
+//       slidesPerView: 4,
+//       spaceBetween: 10
+//     },
+//     // 1150: {
+//     //   slidesPerView: 5,
+//     //   spaceBetween: 20
+//     // },
+
+//   },
+
+//   // Navigation arrows
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+
+// });
