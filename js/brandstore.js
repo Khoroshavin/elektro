8defc28e316b3e82a8b509f1bbdbbf662a320321
Khoroshavin <khoroshavin.russia@gmail.com>
2021-07-23 20:11:53 +0200

    brandstore aeg start

---
+++ js/brandstore.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function() {
+  
+  const brandCategoriesList = new Swiper('.brandstore__categories_list', {
+    // Optional parameters
+    // direction: 'vertical',
+    loop: true,
+    slidesPerView: 5,
+    spaceBetween: 30,
+
+    // pagination
+    // pagination: {
+    //   el: '.swiper-pagination',
+    // },
+  
+    // Navigation arrows
+    // navigation: {
+    //   nextEl: '.swiper-button-next',
+    //   prevEl: '.swiper-button-prev',
+    // },
+  
+
+
+  });
+  
+});
