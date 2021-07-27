3a94fa2913bd2f7617a70e678be1c7adbb2d453b
Khoroshavin <khoroshavin.russia@gmail.com>
2021-07-27 14:23:09 +0200

    brandstore content

--- js/brandstore.js
+++ js/brandstore.js
@@ -6,17 +6,34 @@ document.addEventListener("DOMContentLoaded", function() {
     loop: true,
     slidesPerView: 5,
     spaceBetween: 30,
-
+    breakpoints: {
+      319: {
+        slidesPerView: 2,
+        spaceBetween: 10,
+      },
+      650: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+      850: {
+        slidesPerView: 4,
+        spaceBetween: 20,
+      },
+      1000: {
+        slidesPerView: 5,
+        spaceBetween: 20,
+      },
+    },
     // pagination
     // pagination: {
     //   el: '.swiper-pagination',
     // },
   
     // Navigation arrows
-    // navigation: {
-    //   nextEl: '.swiper-button-next',
-    //   prevEl: '.swiper-button-prev',
-    // },
+    navigation: {
+      nextEl: '.brand_categories_next',
+      prevEl: '.brand_categories_prev',
+    },
   
 
 
